a214d918bda160c8db4dc5855e79f57fc6bd6d66
Kostadin Samardzhiev <k.samardzhiev@gmail.com>
2015-02-04 20:03:28 +0200

    Initial Commit

---
+++ ToDoList/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ToDoList
+//
+//  Created by Kostadin Samardzhiev on 2/4/15.
+//  Copyright (c) 2015 Kostadin Samardzhiev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ToDoList/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ToDoList
+//
+//  Created by Kostadin Samardzhiev on 2/4/15.
+//  Copyright (c) 2015 Kostadin Samardzhiev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
